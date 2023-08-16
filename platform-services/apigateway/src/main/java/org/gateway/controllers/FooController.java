bf50fc5bb9996c78f05c6beab211b3fc5cd32bce
Jose Vicente Ayala Luna <jayala@starbucks.com>
2023-08-16 17:24:26 -0500

    Add UI application

---
+++ platform-services/apigateway/src/main/java/org/gateway/controllers/FooController.java
@@ -0,0 +1,22 @@
+package org.gateway.controllers;
+
+import static org.apache.commons.lang.RandomStringUtils.randomAlphabetic;
+import static org.apache.commons.lang.RandomStringUtils.randomNumeric;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.gateway.model.Foo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FooController {
+
+    @GetMapping("/foos/{id}")
+    public Foo findById(
+            @PathVariable long id, HttpServletRequest req, HttpServletResponse res) {
+        return new Foo(Long.parseLong(randomNumeric(2)), randomAlphabetic(4));
+    }
+
+}
