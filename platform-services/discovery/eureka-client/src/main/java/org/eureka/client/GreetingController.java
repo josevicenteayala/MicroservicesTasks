d892458e4cf06068d17571e02db296ec00e10a04
Jose Vicente Ayala Luna <jayala@starbucks.com>
2023-08-16 16:31:40 -0500

    Add Eureka discovery services

---
+++ platform-services/discovery/eureka-client/src/main/java/org/eureka/client/GreetingController.java
@@ -0,0 +1,9 @@
+package org.eureka.client;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public interface GreetingController {
+
+    @RequestMapping("/greeting")
+    String greeting();
+}
